866585bce30b2ddb87d37a0ab2c106e4226cd315
Raf <rafayel95@gmail.com>
2021-03-19 15:47:31 +0400

    first push

---
+++ public/script.js
@@ -0,0 +1,23 @@
+var socket = io();
+
+socket.emit('joining msg', name);
+
+$('form').submit(function (e) {
+    e.preventDefault();            // will prevent page reloading
+    socket.emit('chat message', (name + ':  ' + $('#m').val()));
+    $('#messages').append($('<li id="list">').text($('#m').val()));
+    $('#messages').append($('<br><br>'))
+    $('#m').val('');
+    return false;
+});
+
+socket.on('chat message', function (msg) {
+    $('#messages').append($('<li>').text(msg));
+});
+
+$("#m").keypress(function (e) {
+    if(e.which === 13 && !e.shiftKey) {
+        e.preventDefault();
+        $(this).closest("form").submit();
+    }
+});

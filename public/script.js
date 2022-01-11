74b6ea13143271e9038f3efc4b45104a7ac556a3
Aram Khachatryan <aramkhachatryan091@gmail.com>
2022-01-11 18:09:52 +0400

    fixed empty message

--- public/script.js
+++ public/script.js
@@ -4,11 +4,13 @@ socket.emit('joining msg', name);
 
 $('form').submit(function (e) {
     e.preventDefault();            // will prevent page reloading
-    socket.emit('chat message', (name + ':  ' + $('#m').val()));
-    $('#messages').append($('<li id="list">').text($('#m').val()));
-    $('#messages').append($('<br><br>'))
-    $('#m').val('');
-    return false;
+    if ($('#m').val()) {
+        socket.emit('chat message', (name + ':  ' + $('#m').val()));
+        $('#messages').append($('<li id="list">').text($('#m').val()));
+        $('#messages').append($('<br><br>'))
+        $('#m').val('');
+        return false;
+    }
 });
 
 socket.on('chat message', function (msg) {
@@ -16,7 +18,7 @@ socket.on('chat message', function (msg) {
 });
 
 $("#m").keypress(function (e) {
-    if(e.which === 13 && !e.shiftKey) {
+    if (e.which === 13 && !e.shiftKey) {
         e.preventDefault();
         $(this).closest("form").submit();
     }
